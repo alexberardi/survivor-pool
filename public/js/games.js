305f4237d7c5d3e0d38d0d98375913fdfaec0306
Jimmy Timmons <jtimmons@workwave.com>
2016-08-16 21:53:34 -0400

    Initial games ajax

---
+++ public/js/games.js
@@ -0,0 +1,46 @@
+
+
+
+(function($, M) {
+	"use strict";
+
+	var settings = {
+		gamesurl: "localhost:3000/games",
+		rowTemplate: "#TableRow",
+		tableAppend: "#schedule",
+	};
+	
+	function getRequest(cURL) {
+		var oReturn = null;
+		
+		$.ajax({
+			type: "GET",
+			url: cURL,
+			async: false
+		})
+		.done(function(oData) {
+			oReturn = oData;
+		});
+
+		return oReturn;
+	}
+	
+	function init() {
+		var games = getRequest(settings.gamesurl);
+		console.log(games);
+		$(games).each(function() {
+			var game = {
+				date: $(this).find("gameDate").text(),
+				hometeam: $(this).find("homeTeamName").text(),
+				hometeamscore: $(this).find("homeScore").text(),
+				awayteam: $(this).find("awayTeamName").text(),
+				awayteamscore: $(this).find("awayScore").text()
+			};
+
+			$(settings.tableAppend).append(M.to_html($(settings.rowTemplate).html(), game));
+		});
+	}
+	
+	init();
+
+}(jQuery, Mustache));
