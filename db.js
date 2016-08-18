e426de491b84fee17bda74138afd6833e634a6f7
Alex Berardi <AlexmBerardi@gmail.com>
2016-08-17 23:14:27 -0400

    testing

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
-	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		'dialect': 'postgres'
-	});
+	// sequelize = new Sequelize(process.env.DATABASE_URL, {
+	// 	'dialect': 'postgres'
+	// });
 } else{
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
