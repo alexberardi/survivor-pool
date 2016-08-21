f26a7a764f0495571568ac725cb318a7556cad3d
Jimmy Timmons <jtimmons@workwave.com>
2016-08-20 20:26:18 -0400

    Separate schedules to home page

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,21 @@
 $(function () {
-	$('#login-button').click(function (e) {
+	$('#login-form').on('valid submit', function (e) {
 		var user = {
 			"email": $("#login-email").val(),
 			"password": $("#login-pass").val()
 		}
 
-		if(user.email.length > 0 && user.password.length > 0){
-			$.ajax({
-			type: "POST",
-			url: 'users/login',
-			contentType : 'application/json',
-			data: JSON.stringify(user),
-			async: false
-			})
-			.done(function(data, textStatus, request){
-				document.cookie = "Auth="+ request.getResponseHeader('Auth');
-				$('login-pass').val('');
-				$('login-email').val('');
-				$("#login").hide();
-				$("#logout").show();
-
-			});
-		} else if (user.email.length === 0 && user.email.password === 0) {
-			alert("Email and password are required");
-		} else if (user.email.length === 0) {
-			alert("Email is required.");
-		} else {
-			alert("Password is required");
-		}
-	});
+		$.ajax({
+		type: "POST",
+		url: 'users/login',
+		contentType : 'application/json',
+		data: JSON.stringify(user),
+		async: false
+		})
+		.done(function(data, textStatus, request){
+			document.cookie = "Auth="+ request.getResponseHeader('Auth');
+			$('login-pass').val('');
+			$('login-email').val('');
+		});
+	})
 });
