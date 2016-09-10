631024356a76860d8bfd22bce432a62fe5e1099d
Jimmy Timmons <jtimmons@workwave.com>
2016-09-09 21:26:59 -0400

    Reverting, RIP bar chart

--- public/js/standings.js
+++ public/js/standings.js
@@ -8,44 +8,9 @@
 	};
 	
 	
-	function createChart(data) {
-		var chartCanvas = $("#bar-chart");
-		var standingsChart = new Chart(chartCanvas, {
-			type: 'horizontalBar',
-			data: data,
-			options: {
-				legend: {
-					display: false
-				},
-				tooltips: {
-					backgroundColor: 'rgba(54, 44, 106, 1)'
-				},
-				scales: {
-					xAxes: [{
-						ticks: {
-							beginAtZero: true,
-							stepSize: 1
-						}
-					}],
-					yAxes: [{
-						categoryPercentage: .5,
-						gridLines : {
-							drawTicks: false,
-						}
-					}]
-
-				}
-			}
-		});
-	}
-	
 	function init() {
 		var standings = getRequest(settings.standingsURL);
-		var barPlayers = [];
-		var barData = [];
-		var barColors = [];
-		var borderColors = [];
-		
+
 		standings = standings[0];
 		standings.forEach(function(standing) {
 			var standingsInfo = {
@@ -54,27 +19,10 @@
 				playername: standing.first + ' ' + standing.last
 			};
 
-			barPlayers.push(standingsInfo.teamname + " ( " + standingsInfo.playername + " ) ");
-			barData.push(standingsInfo.streak);
-			barColors.push(randomColor({hue: 'purple', luminosity: 'dark', format: 'rgba'}));
-			borderColors.push(randomColor({hue: 'purple', format: 'rgba'}));
 
 			$(settings.tableAppend).append(M.to_html($(settings.rowTemplate).html(), standingsInfo));
 		});
 
-		var data = {
-			labels: barPlayers,
-			datasets: [{
-				label: "Correct Picks",
-				data: barData,
-				backgroundColor: barColors,
-				borderColor: borderColors,
-				borderWidth: 3,
-				hoverBorderColor: 'rgba(54, 44, 106, 1)',
-
-			}]
-		};
-		createChart(data);
 	}
 	
 	init();
