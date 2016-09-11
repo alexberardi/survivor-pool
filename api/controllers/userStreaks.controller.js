5d28b26ef0d9b0aa572eb46159f8d6416feb3fdb
Jimmy Timmons <jtimmons@workwave.com>
2016-09-11 17:43:57 -0400

    Boolean

--- api/controllers/userStreaks.controller.js
+++ api/controllers/userStreaks.controller.js
@@ -39,7 +39,7 @@ var getStandings = function(req, res) {
 
 
 var getCountActive = function(req, res) {
-    db.sequelize.query("SELECT COUNT(*) FROM userStreaks WHERE current = 1", { type: db.sequelize.QueryTypes.SELECT})
+    db.sequelize.query("SELECT COUNT(*) FROM userStreaks WHERE current = true", { type: db.sequelize.QueryTypes.SELECT})
         .then(function(streaks){
             res.json(streaks);
         })
