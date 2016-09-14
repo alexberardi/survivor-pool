d564cc417286610cfc4db918b36fb6898925389b
Alex Berardi <AlexmBerardi@gmail.com>
2016-09-13 22:20:30 -0400

    Testing schema change without force:true

--- models/teams.js
+++ models/teams.js
@@ -10,6 +10,10 @@ module.exports = function(sequelize, DataTypes){
 		teamcity: {
 			type: DataTypes.STRING,
 			allowNull: false
+		},
+		teamtype: {
+			type: DataTypes.STRING,
+			allowNull: true
 		}
 	});
 };
