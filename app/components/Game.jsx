fe7b96c80ce50ef4d4506f6d92cb3de737858547
Jimmy Timmons <jtimmons@workwave.com>
2017-08-09 19:50:15 -0400

    Games Work / Style Tweaks

--- app/components/Game.jsx
+++ app/components/Game.jsx
@@ -6,22 +6,44 @@ class Game extends Component {
 		super(props);
     }
 	render() {
-        return (
+		let kickoffDate = this.props.gameDate.substring(4,6) + "/" + this.props.gameDate.substring(6,8) + "/" + this.props.gameDate.substring(0,4); 
+		let gameID = this.props.gameID;
+		let quarter = this.props.quarter;
+		let awayImage = `/images/${this.props.awayTeamName.toLowerCase()}.gif`;
+		let homeImage = `/images/${this.props.homeTeamName.toLowerCase()}.gif`;
+
+		switch(quarter) {
+			case "P":
+				quarter = "Pregame";
+				break;
+			case "H":
+				quarter = "Half-Time";
+				break;
+			case "F":
+				quarter = "Final";
+				break;
+			case "FO":
+				quarter = "Final / Overtime";
+			default:
+				quarter = 'Q' + quarter;
+		}
+		
+		return (
             <div className="pick-card-row">
 				<div className="pick-card">
 					<div className="pick-card-title">
-						<img src="/images/49ers.gif" height="50" width="50"/>
+						<img src={awayImage} height="50" width="50"/>
 						<span className="vs-span">AT</span>
-						<img src="/images/bears.gif" height="50" width="50"/>
+						<img src={homeImage} height="50" width="50"/>
 					</div>
 					<div className="pick-card-content">
 						<div className="pick-card-kickoff">
                             <div className="pick-card-info-title">Kickoff:</div>
-							Monday 9.11.17 4PM
+							{kickoffDate} at {this.props.time}
 						</div>
                         <div className="pick-card-score">
                             <div className="pick-card-info-title">Current Score:</div>
-                            11 - 7 3rd Quarter 
+                            {this.props.awayScore} - {this.props.homeScore} | {quarter}
                         </div>
 					</div>
 				</div>
--- app/components/Picks.jsx
+++ app/components/Picks.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import * as Redux from 'react-redux';
+import * as actions from 'actions';
+import * as Requests from 'Requests';
 
 import Nav from 'Nav';
 import Game from 'Game';
@@ -8,26 +10,60 @@ import Footer from 'Footer';
 class Picks extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {uid: null, games: null, startedGames: null};
     }
 	componentDidMount() {
-		/*
-		get game info for the week
-		games.map((game) => {
-				return (
-					<Game id={game.id} {...game}/>
-				)
+		var {dispatch} = this.props;
+		var {uid, displayName} = dispatch(actions.getUserAuthInfo());
+
+		var that = this;
+
+		//Get Games
+		Requests.post('/games/update', {}).then(function(response) {
+			console.log('Updated Games');
+			Requests.get(`/games/user/${uid}`).then(function(games) {
+				console.log('Fetched Games');
+				let currentWeekGames = games.data;
+				Requests.get('/games/started').then(function(started) {
+					let startedGames = started.data;
+					console.log('Fetched Started Games');
+					that.setState({games: currentWeekGames, startedGames});
+				});
 			});
-		*/
+		});
 	}
 	render() {
+		let games = this.state.games;
+		let started = this.state.startedGames;
+		let week = games !== null ? games[0].week : 0;
+
+		var renderGames = () => {
+			if(games === null || games.length == 0) {
+				return (
+					 <div className="pick-card-row">
+						<div className="pick-card">
+							<div className="pick-card-content">
+								 Loading Games...
+							</div>
+						</div>
+					</div>
+				)
+			}
+			return games.map((game) => {
+				return (
+					<Game key={game.gameID} {...game}/>
+				)
+			})
+		}
+
 		return (
 			<div className="dashboard">
 				<Nav page={'Picks'} />
 				<div className="row">
 					<div className="column small-centered small-11 medium-10 large-9">
-						<div className="dashboard-title">Picks</div>
+						<div className="dashboard-title">Week {week}</div>
 						<div className="container">
-							<Game />
+							{renderGames()}
 						</div>
 					</div>
 				</div>
