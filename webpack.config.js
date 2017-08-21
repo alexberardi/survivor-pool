304cc2a9f7765bc5362d757c82d0210c3641f8f7
Alex Berardi <alexmberardi@gmail.com>
2017-08-21 19:14:45 -0400

    Changing DATABASE_URL to FIREBASEDATABASE_URL so it doesn't clash with regular DATABASE_URL env variable on heroku.

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         API_KEY: JSON.stringify(process.env.API_KEY),
         AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
-        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        DATABASE_URL: JSON.stringify(process.env.FIREBASEDATABASE_URL),
         STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
         MESSAGE_SENDER: JSON.stringify(process.env.MESSAGE_SENDER)
       }
