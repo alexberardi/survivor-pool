9e566fc52df381895fcd33cfe44d685f0f6a8855
Alex Berardi <alexmberardi@gmail.com>
2017-08-21 19:16:09 -0400

    Fixing another spot

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -4,7 +4,7 @@ try {
 	var config = {
   	apiKey: process.env.API_KEY,
   	authDomain: process.env.AUTH_DOMAIN,
-  	databaseURL: process.env.DATABASE_URL,
+  	databaseURL: process.env.FIREBASEDATABASE_URL,
   	storageBucket: process.env.STORAGE_BUCKET,
   	messagingSenderId: process.env.MESSAGE_SENDER
 	};
