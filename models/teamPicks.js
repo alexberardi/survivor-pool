8d60d1db543aa5b27a8d5637f8cc06a0bde153ab
Jimmy Timmons <jtimmons@workwave.com>
2017-08-22 18:50:28 -0400

    UserID => varchar

--- models/teamPicks.js
+++ models/teamPicks.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes){
         },
         userID: {
             field: 'userID',
-            type: DataTypes.INTEGER,
+            type: DataTypes.STRING,
             allowNull: false
         },
         teamID: {
