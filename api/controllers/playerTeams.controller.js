0576755ac948ec2a3dcd90888f8d7e5b9183bc75
Alex Berardi <alexmberardi@gmail.com>
2017-08-30 19:42:44 -0400

    Adding fix for duplicate team names on insert and update

--- api/controllers/playerTeams.controller.js
+++ api/controllers/playerTeams.controller.js
@@ -1,5 +1,6 @@
 var _ = require('underscore');
 var db = require('../../db');
+var uuid = require('uuid');
 
 var deleteTeam = function(req, res) {
     var team_id = req.params.team_id;
@@ -102,25 +103,77 @@ var updateTeamPaid = function(req, res) {
     });
 }
 
-var teamCreate = function(req,res){
+function checkTeamName(body) {
+    return new Promise(function(resolve, reject){
 
-    var dateCutOff = new Date('2017-09-11 22:20:00');
-    
-    if (new Date() < dateCutOff) {
-        var body = _.pick(req.body, 'team_name', 'user_id');
-        db.playerTeams.create(body)
-            .then(function(team) {
-                body = Object.assign(body, {team_id: team.team_id, total:0, current: true});
-                db.teamStreaks.create(body)
-                    .then(function(streak){
-                        res.json(team);
-                    })
+        var whereObj;
 
+        if (body.team_id) {
+            db.playerTeams.findAndCountAll({
+                where: {
+                    team_name: body.team_name,
+                    team_id: {$ne: body.team_id}
+                }
             })
-            .catch(function(e) {
-                console.log(e);
-                res.status(400).json(e);
-            });
+            .then(function(result){
+                if (result.count > 0) {
+                    if (body.team_name.length > 220) {
+                        res.status(401).json({
+                            error: 'Team name is too long. Please shorten.'
+                        });
+                    } else {
+                        body.team_name = body.team_name + ' ' + uuid.v4();;
+                    } 
+                    resolve("202");              
+                } else{
+                    resolve("200");
+                }                
+            })            
+        } else {
+            db.playerTeams.findAndCountAll({
+                where: {
+                    team_name: body.team_name
+                }
+            })
+            .then(function(result){
+                if (result.count > 0) {
+                    if (body.team_name.length > 220) {
+                        res.status(401).json({
+                            error: 'Team name is too long. Please shorten.'
+                        });
+                    } else {
+                        body.team_name = body.team_name + ' ' + uuid.v4();;
+                    } 
+                    resolve("202");              
+                } else{
+                    resolve("200");
+                }
+                
+            })
+        }
+
+        
+    })
+}
+
+var teamCreate = function(req,res){
+    var dateCutOff = new Date('2017-09-11 22:20:00');    
+    if (new Date() < dateCutOff) {
+            var body = _.pick(req.body, 'team_name', 'user_id');
+            checkTeamName(body).then(function(responseStatus){
+                db.playerTeams.create(body)
+                    .then(function(team) {
+                        body = Object.assign(body, {team_id: team.team_id, total:0, current: true});
+                        db.teamStreaks.create(body)
+                            .then(function(streak){
+                                res.status(responseStatus).json(team);
+                            })
+                    })
+                    .catch(function(e) {
+                        console.log(e);
+                        res.status(400).json(e);
+                    });
+                })
     } else {
         var e = {error: 'The cutoff time to add a team has passed.'};
         res.status(401).json(e);
@@ -135,28 +188,29 @@ var updateTeamName = function(req, res) {
         res.status(401).send();
         return;
     }
+    checkTeamName(body).then(function(stuff){
+        var attributes = { team_name : body.team_name};
 
-    var attributes = { team_name : body.team_name};
-
-    db.playerTeams.findOne({
-        where: {
-            team_id: team_id
-        }
-    })
-    .then(function(team) {
-        if (team) {
-            return team.update(attributes)
-                .then(function(team){
-                    res.json(team.toJSON());
-                }, function(e){
-                    res.status(400).json(e);
-                });
-        } else {
-            res.status(404).send();
-        }
-    }, function(){
-        res.status(500).send();
-    });
+        db.playerTeams.findOne({
+            where: {
+                team_id: team_id
+            }
+        })
+        .then(function(team) {
+            if (team) {
+                return team.update(attributes)
+                    .then(function(team){
+                        res.json(team.toJSON());
+                    }, function(e){
+                        res.status(400).json(e);
+                    });
+            } else {
+                res.status(404).send();
+            }
+        }, function(){
+            res.status(500).send();
+        });
+    });        
 };
 
 module.exports = {
