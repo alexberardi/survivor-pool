296649db82f68b2b2e8fc2722738df329e04cb20
Jimmy Timmons <jtimmons@workwave.com>
2017-09-18 19:23:31 -0400

    Removing debugging

--- app/components/PieChartBuilder.jsx
+++ app/components/PieChartBuilder.jsx
@@ -63,9 +63,7 @@ class PieChartBuilder extends Component {
     }
 	render() {
         if(this.props.data !== null) {
-            console.log(this.props.data, 'data');
             const data = this.PieChartFormatter(this.props.data, this.props.type);
-            console.log(data, 'formattedData');
             return  (
                 <PieChart width={200} height={250}>
                     <Pie
